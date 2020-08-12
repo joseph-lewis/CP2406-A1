e029253f7b23ba1e8fa430f9aee460c6f0a5d2bc
Joe <jlewi142@gmail.com>
2020-08-12 12:06:48 +1000

    Add min and max functions, to calculate all the data we need

--- src/RainfallAnalyser.java
+++ src/RainfallAnalyser.java
@@ -12,16 +12,23 @@ import java.util.List;
 
 public class RainfallAnalyser {
     public static List<String> OUTPUT = new ArrayList<>();
-    public static List<Float> RAINFALL_TOTALS = new ArrayList<>();
+    public static List<Double> RAINFALL_TOTALS = new ArrayList<>();
+    public static List<Double> RAINFALL_MIN = new ArrayList<>();
+    public static List<Double> RAINFALL_MAX = new ArrayList<>();
+
 
     public static void main(String[] args) {
         String dbPath = "MountSheridanStationCNS.csv";
         readCSV(dbPath); //Read the file, and unload data into OUTPUT list variable
-        calculateMonthlyRain(); //Calculate total rainfall per month
-//        calculateMinRain(); //Calculate minimum daily rainfall per month
-//        calculateMaxRain(); //Calculate maximum daily rainfall per month
+        calculateRainStats(); //Calculate the monthly rainfall statistics
+        for(int i = 0; i < RAINFALL_TOTALS.size(); i++){
+            double totalRain = RAINFALL_TOTALS.get(i);
+            double minRain = RAINFALL_MIN.get(i);
+            double maxRain = RAINFALL_MAX.get(i);
+
+            System.out.printf("Month - %s, Total - %.2f, Min - %.2f, Max - %.2f\n", i+1, totalRain, minRain, maxRain);
+        }
 //        writeCSV(); //Write the new calculated data into new CSV file
-        System.out.println(RAINFALL_TOTALS);
 
     }
 
@@ -40,35 +47,55 @@ public class RainfallAnalyser {
         }
     }
 
-    private static void calculateMonthlyRain() {
+    private static void calculateRainStats() {
         int currentMonth = 0;
-        float currentMonthRain = 0;
+        double currentMonthRain = 0;
         double rainfallAmount;
+        double minRainfall = 0;
+        double maxRainfall = 0;
         int rainfallMonth;
+
 
         for(int i=1; i < OUTPUT.size(); i++){
             String[] tempLine = (OUTPUT.get(i)).split(",");
             try{
                 rainfallAmount = Double.parseDouble(tempLine[5]);
             } catch (Exception e) {
-                rainfallAmount = 0;
+                rainfallAmount = 0; //If there is an error, means there was no rain on that day
             }
             rainfallMonth = Integer.parseInt(tempLine[3]);
 
             if(currentMonth != rainfallMonth){
-                if(currentMonth != 0){
-                    RAINFALL_TOTALS.add(currentMonthRain);
+                if(currentMonth != 0){ //If it is a new month, add current month data, and reset temp variables
+                    UpdateConstants(currentMonthRain, minRainfall, maxRainfall);
                     currentMonthRain = 0;
+                    minRainfall = 0;
+                    maxRainfall = 0;
                 }
                 currentMonth = rainfallMonth;
             }
 
             currentMonthRain += rainfallAmount;
 
+            if(rainfallAmount > 0){ //If there was any rain, see if it is the new min or max for the month
+                if(minRainfall == 0){ //Set the new minimum
+                    minRainfall = rainfallAmount;
+                }
+                if(rainfallAmount < minRainfall ){ //Is the rain the new min?
+                    minRainfall = rainfallAmount;
+                }
+                if(rainfallAmount > maxRainfall){ //Is the rain the new max?
+                    maxRainfall = rainfallAmount;
+                }
+            }
         }
+        UpdateConstants(currentMonthRain, minRainfall, maxRainfall);
     }
 
-    private static void calculateMinRain() {
+    private static void UpdateConstants(double currentMonthRain, double minRainfall, double maxRainfall){
+        RAINFALL_TOTALS.add(currentMonthRain);
+        RAINFALL_MIN.add(minRainfall);
+        RAINFALL_MAX.add(maxRainfall);
     }
 
     private static void calculateMaxRain() {
